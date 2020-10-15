1062aab840d2e1c51266760d00689d5f9fbe0567
Александр Оруджев <stvort@mail.ru>
2020-10-15 16:51:06 +0400

    another small fix

--- ctx-listener-demo/src/main/java/ru/otus/ctxlistenerdemo/infrastructure/OnStartupAnnotationProcessor.java
+++ ctx-listener-demo/src/main/java/ru/otus/ctxlistenerdemo/infrastructure/OnStartupAnnotationProcessor.java
@@ -1,8 +1,8 @@
 package ru.otus.ctxlistenerdemo.infrastructure;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import ru.otus.ctxlistenerdemo.api.OnStartup;
 
@@ -10,12 +10,12 @@ import java.util.Arrays;
 
 @RequiredArgsConstructor
 @Component
-public class OnStartupAnnotationProcessor implements ApplicationListener<ContextRefreshedEvent> {
+public class OnStartupAnnotationProcessor {
 
     private final BeanDefinitionHelper helper;
 
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent evt) {
+    @EventListener(ContextRefreshedEvent.class)
+    public void onApplicationEvent() {
         helper.getBeansContextList().forEach(bc ->
                 Arrays.stream(bc.getBeanClass().getDeclaredMethods())
                         .filter(m -> m.isAnnotationPresent(OnStartup.class))
